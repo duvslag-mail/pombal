a7c1402337bb21419779a689077ca1faa335a47c
ayo <hey@ayoreis.com>
2026-09-20 22:44:39 +0100

    Make the NixOS module a service and configure Postfix

--- pombal.nix
+++ pombal.nix
@@ -1,6 +1,37 @@
-{
-	services = {
-		postfix.enable = true;
-		dovecot2.enable = true;
+{ config, lib, ... }: let
+	cfg = config.services.pombal;
+in {
+	options.services.pombal = {
+		enable = lib.mkEnableOption "Pombal mail server";
+
+		fqdn = lib.mkOption {
+			type = lib.types.str;
+		};
+	};
+
+	config = lib.mkIf cfg.enable {
+		networking.firewall.allowedTCPPorts = [
+			# SMTP
+			25
+			# SMTP with opportunistic TLS (STARTTLS)
+			587
+			# SMTPS
+			465
+		];
+
+		services = {
+			postfix = {
+				enable = true;
+				enableSubmission = true;
+				enableSubmissions = true;
+
+				settings.main = {
+					myorigin = "$mydomain";
+					myhostname = cfg.fqdn;
+				};
+			};
+
+			dovecot2.enable = true;
+		};
 	};
 }
